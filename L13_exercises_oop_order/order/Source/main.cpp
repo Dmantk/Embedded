63cd64b3fa270dfeb42eef9b98b96c76ff7ef633
Dmantk <133779334+Dmantk@users.noreply.github.com>
2023-07-07 21:11:03 +0700

    Update main.cpp

--- L13_exercises_oop_order/order/Source/main.cpp
+++ L13_exercises_oop_order/order/Source/main.cpp
@@ -1,7 +1,7 @@
 /*
  * File: order
  * Author: Duong Minh An
- * Date: 28/07/2023
+ * Date: 07/07/2023
  * Description: This's an example about create an order interface for managers and employees
  */
 #include "order.h"
