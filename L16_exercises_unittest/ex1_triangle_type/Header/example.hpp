c28411d96f9c295d254de4bde208dfb202ea98b2
Dmantk <133779334+Dmantk@users.noreply.github.com>
2023-08-18 22:34:33 +0700

    Update File

---
+++ L16_exercises_unittest/ex1_triangle_type/Header/example.hpp
@@ -0,0 +1,13 @@
+#ifndef __EXAMPLE_HPP__
+#define __EXAMPLE_HPP__
+
+#include "example.hpp"
+#include <string>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+string determineTriangleType(double x1, double y1, double x2, double y2, double x3, double y3);
+
+#endif
